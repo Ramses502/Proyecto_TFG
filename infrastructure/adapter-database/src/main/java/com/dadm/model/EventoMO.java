026c65a50f1969f5fd2dca6cac0f87dec1c331e1
rcuartba <rcuartba@mercadona.es>
2023-08-08 17:18:38 +0200

    Agregado EventMO

---
+++ infrastructure/adapter-database/src/main/java/com/dadm/model/EventoMO.java
@@ -0,0 +1,35 @@
+package com.dadm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "Evento")
+public class EventoMO {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "date")
+    private LocalDate date;
+    @Column(name = "capacity")
+    private Integer capacity;
+}
