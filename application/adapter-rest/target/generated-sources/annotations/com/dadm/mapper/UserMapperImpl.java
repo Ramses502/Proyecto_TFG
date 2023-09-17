e37502f3c9dc503201d5eebdf61efbc0c76c7964
Ramses502 <ramses21793@gmail.com>
2023-09-17 18:15:47 +0200

    sfgsdfgsdfg

--- application/adapter-rest/target/generated-sources/annotations/com/dadm/mapper/UserMapperImpl.java
+++ application/adapter-rest/target/generated-sources/annotations/com/dadm/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-17T18:06:34+0200",
+    date = "2023-09-17T18:15:36+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class UserMapperImpl implements UserMapper {
